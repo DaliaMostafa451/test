3f824db5e0947dbf599bdd068bcc73faef32d50e
DaliaMostafa451 <dalymost0@gmail.com>
2025-04-11 22:35:50 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,216 @@
-import 'package:flutter/material.dart';
+   import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(pointsCounter());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class pointsCounter extends StatefulWidget {
+  @override
+  State<pointsCounter> createState() => _pointsCounterState();
+}
+
+class _pointsCounterState extends State<pointsCounter> {
+  int teamB = 0;
+
+  int teamA = 0;
+
+  void addonepoint() {}
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          title: Text('points counter'),
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      'Team A',
+                      style: TextStyle(
+                        fontSize: 32,
+                      ),
+                    ),
+                    Text(
+                      '$teamA',
+                      style: TextStyle(
+                        fontSize: 150,
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orange,
+                          minimumSize: Size(150, 50)),
+                      onPressed: () {
+                        setState(() {
+                          teamA++;
+                        });
+                        print(teamA);
+                      },
+                      child: Text(
+                        'Add 1 point',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orange,
+                          minimumSize: Size(150, 50)),
+                      onPressed: () {
+                        setState(() {
+                          teamA += 2;
+                          ;
+                        });
+                      },
+                      child: Text(
+                        'Add 2 point',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orange,
+                          minimumSize: Size(150, 50)),
+                      onPressed: () {
+                        setState(() {
+                          teamA += 3;
+                        });
+                      },
+                      child: Text(
+                        'Add 3 point',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 400,
+                  child: VerticalDivider(
+                    color: Color(0xff1b0909),
+                    thickness: 1,
+                  ),
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'Team B',
+                      style: TextStyle(
+                        fontSize: 32,
+                      ),
+                    ),
+                    Text(
+                      '$teamB',
+                      style: TextStyle(
+                        fontSize: 150,
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orange,
+                          minimumSize: Size(150, 50)),
+                      onPressed: () {
+                        setState(() {
+                          teamB++;
+                        });
+                      },
+                      child: Text(
+                        'Add 1 point',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orange,
+                          minimumSize: Size(150, 50)),
+                      onPressed: () {
+                        setState(() {
+                          teamB += 2;
+                        });
+                      },
+                      child: Text(
+                        'Add 2 point',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orange,
+                          minimumSize: Size(150, 50)),
+                      onPressed: () {
+                        setState(() {
+                          teamB += 3;
+                        });
+                      },
+                      child: Text(
+                        'Add 3 point',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 36,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange, minimumSize: Size(150, 50)),
+              onPressed: () {
+                setState(() {
+                  teamA = 0;
+                  teamB = 0;
+                });
+              },
+              child: Text(
+                'Reset',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
